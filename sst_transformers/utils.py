5925f06c93f01515f6c99582a84d38ea94b0466f
Zeeshan Ahmad <zeeshanahmad10809@gmail.com>
2020-08-16 21:39:11 +0500

    commented learning params

--- sst_transformers/utils.py
+++ sst_transformers/utils.py
@@ -8,9 +8,34 @@ def get_binary_label(sentiment):
 
 
 def transformer_params(name):
-    if name == 'bert':
+    '''if name == 'bert-base-uncased' or name == 'bert-large-uncased':
         return {'batch_size': 32,
                 'learning_rate': 5e-5}
+    elif name == 'roberta-base' or name == 'roberta-large':
+        return {'batch_size': 32,
+                'learning_rate': 2e-5}
+    elif name == 'distilbert':
+        return {'batch_size': 32,
+                'learning_rate': 1e-5}
+    elif name == 'albert-xlarge' or name == 'albert-xxlarge':
+        return {'batch_size': 32,
+                'learning_rate': 1e-5}
+    elif name == 'xlmroberta-base' or name == 'xlmroberta-large':
+        return {'batch_size': 32,
+                'learning_rate': 1e-5}
+    elif name == 'electra-small' or name == 'electra-large':
+        return {'batch_size': 32,
+                'learning_rate': 1e-5}
+    elif name == 'mobilebert':
+        return {'batch_size': 32,
+                'learning_rate': 1e-5}
+    elif name == 'gpt2-medium':
+        pass
+    '''
+    return {'batch_size': 32,
+            'learning_rate': 1e-5}
+    '''
     else:
         raise ValueError
+    '''
 
