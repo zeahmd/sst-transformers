9fb05a11c44d1e096532cfc3ffaa4643b6531622
Zeeshan Ahmad <zeeshanahmad10809@gmail.com>
2020-08-18 02:31:46 +0500

    changed default epochs

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.option("-n", "--name", default='bert-base', help='model name')
 @click.option("-r", "--root", is_flag=True, help='SST root or all')
 @click.option("-b", "--binary", is_flag=True, help='SST binary or fine')
-@click.option("-e", "--epochs", default=7, help="no of training iterations/epochs")
+@click.option("-e", "--epochs", default=15, help="no of training iterations/epochs")
 @click.option("-p", "--patience", default=np.inf, help='patience for early stopping')
 @click.option("-s", "--save", is_flag=True, help="save model")
 def run(name, root, binary, epochs, patience, save):
